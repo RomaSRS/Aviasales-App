3afba4ef0a61f0cf9b0b2f7d535eb7922a10c61d
romasrs <shaman.roman@tut.by>
2021-03-29 23:28:52 +0300

    feat: implement hooks in component and fix bugs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,14 +6,12 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { getTickets } from "../../redux/actions";
 import * as actions from "../../redux/actions";
-import checkFilter from "../../utilsfilter";
 import Header from "../Header/Header";
 import Loading from "../Loading/Loading";
 import NoTickets from "../NoTickets/NoTickets";
 import SortingTabs from "../SortingTabs/SortingTabs";
 import CheckboxFilter from "../CheckboxFilter/CheckboxFilter";
-// import TicketList from "../TicketList/TicketList";
-import Ticket from "../Ticket/Ticket";
+import TicketList from "../TicketList/TicketList";
 import styles from "./App.module.scss";
 
 const App = () => {
@@ -23,49 +21,15 @@ const App = () => {
   const sortingParam = useSelector((state) => state.sortBy);
   const stopsFilterValue = useSelector((state) => state.filter.stops);
   const isError = useSelector((state) => state.isError);
-  const myFilter = useSelector((state) => state.filter);
-  const { without, one, two, three } = myFilter;
-  const { cheaply, faster, ticketsForView } = useSelector((state) => state.tabTickets);
-
+  
   useEffect(() => {
     dispatch(getTickets());
   }, []);
 
-  // eslint-disable-next-line
-  if (cheaply) {
-    // eslint-disable-next-line
-    tickets.sort((prev, next) => {
-      if (prev.price < next.price) return -1;
-      if (prev.price < next.price) return 1;
-    });
-  }
-  if (faster) {
-    // eslint-disable-next-line
-    tickets.sort((prev, next) => {
-      if (prev.segments[0].duration < next.segments[0].duration) return -1;
-      if (prev.segments[0].duration < next.segments[0].duration) return 1;
-    });
-  }
-
-  const arr = checkFilter(without, one, two, three, tickets);
-  let key = 1;
-
-    // eslint-disable-next-line
-    const ticketList =
-    arr.length &&
-    // eslint-disable-next-line
-    arr.map((ticket, i) => {
-      key += 1;
-      if (i < ticketsForView) {
-        return <Ticket key={key} ticket={ticket} />;
-      }
-    });
-
-  
   const isLoading = fetchingState === "fetching";
 
-  const someValue = useMemo(() => !ticketList.length && !isLoading && !isError, [
-    ticketList,
+  const someValue = useMemo(() => !tickets && !isLoading && !isError, [
+    tickets,
     isLoading,
     isError,
   ]);
@@ -89,23 +53,12 @@ const App = () => {
               handleTabChange={() => dispatch(actions.getCheaplyTickets)}
             />
             {someValue && <NoTickets /> }
-            {ticketList}
-            {/* <TicketList
+            {tickets}
+            <TicketList
               tickets={tickets}
               isLoading={isLoading}
               handleShowMore={() => dispatch(actions.getTickets)}
-            /> */}
-            <div className={styles.root}>
-              {ticketList.length && (
-              <button
-                className={styles.button}
-                type="button"
-                onClick={() => dispatch(actions.getTickets())}
-              >
-                ПОКАЗАТЬ ЕЩЕ
-              </button>
-              )}
-            </div>
+            />
           </section>
         </div>
       </div>
--- src/components/TicketList/TicketList.jsx
+++ src/components/TicketList/TicketList.jsx
@@ -4,45 +4,49 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { handleShowMore } from "../../redux/actions";
-// import Ticket from "../Ticket/Ticket";
-// import checkFilter from "../../utilsfilter";
+import Ticket from "../Ticket/Ticket";
+import checkFilter from "../../utilsfilter";
 
 import styles from "./TicketList.module.scss";
 
 const TicketList = () => {
   const tickets = useSelector((state) => state.tickets);
-  // const { cheaply, faster } = useSelector((state) => state.tabTickets);
+  const { cheaply, faster, ticketsForView } = useSelector( (state) => state.tabTickets);
+
+  const myFilter = useSelector((state) => state.filter);
+  const { without, one, two, three } = myFilter;
+
   const dispatch = useDispatch();
 
-  // if (cheaply) {
-  //   tickets.sort((prev, next) => {
-  //     if (prev.price < next.price) return -1;
-  //     if (prev.price < next.price) return 1;
-  //   });
-  // }
-  // if (faster) {
-  //   tickets.sort((prev, next) => {
-  //     if (prev.segments[0].duration < next.segments[0].duration) return -1;
-  //     if (prev.segments[0].duration < next.segments[0].duration) return 1;
-  //   });
-  // }
+  if (cheaply) {
+    tickets.sort((prev, next) => {
+      if (prev.price < next.price) return -1;
+      if (prev.price < next.price) return 1;
+    });
+  }
+  if (faster) {
+    tickets.sort((prev, next) => {
+      if (prev.segments[0].duration < next.segments[0].duration) return -1;
+      if (prev.segments[0].duration < next.segments[0].duration) return 1;
+    });
+  }
 
-  // const arrs = checkFilter(without, one, two, three, tickets);
-  // let key = 1;
+  const arrs = checkFilter(without, one, two, three, tickets);
+  let key = 1;
 
-  // /* eslint-disable-next-line */
-  // const ticketList =
-  //   arrs.length &&
-  //   arrs.map((ticket, i) => {
-  //     key += 1;
-  //     if (i < ticketsForView) {
-  //       return <Ticket key={key} ticket={ticket} />;
-  //     }
-  //   });
+  /* eslint-disable-next-line */
+  const ticketList =
+    arrs.length &&
+    arrs.map((ticket, i) => {
+      key += 1;
+      if (i < ticketsForView) {
+        return <Ticket key={key} ticket={ticket} />;
+      }
+    });
 
   return (
     <div className={styles.root}>
-      {/* {ticketList} */}
+      {ticketList}
       {tickets.length && (
         <button
           className={styles.button}
